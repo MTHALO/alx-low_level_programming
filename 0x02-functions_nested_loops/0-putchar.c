5e24814c62c087af271ec5b52a7845c6457cd48a
MTHALO <mohammed.tegegne.35@gmail.com>
2022-07-06 02:56:08 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,23 +5,19 @@
 * Return: Always 0 (Success)
 */
 
-int _putchar(char c);
+int main(void)
+
 {
-	_putchar("_");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	_putchar("p");
+	return (0);
 
-	_putchar("u");
-
-	_putchar("t");
-
-	_putchar("c");
-
-	_putchar("h");
-
-	_putchar("a");
-
-	_putchar("r");
-
-	_putchar("\n");
 }
