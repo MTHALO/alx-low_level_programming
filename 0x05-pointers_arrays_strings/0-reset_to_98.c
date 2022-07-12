f49969b7c6ad3ee54e8e50489e9017851bc1d205
MTHALO <mohammed.tegegne.35@gmail.com>
2022-07-12 03:30:10 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * main - takes a pointer to an int as parameter and updates the value
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * n: pointer
  * Return: Always 0.
  */
 void reset_to_98(int *n)
