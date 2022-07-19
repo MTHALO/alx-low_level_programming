c22d32ec119770f3e1f46d0ee5e249ae63cbdb96
MTHALO <mohammed.tegegne.35@gmail.com>
2022-07-19 04:32:12 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @src: memory area
+ * @dest: destination memory area
+ * @n: bytes filled
+ * Return: *s dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
+}
