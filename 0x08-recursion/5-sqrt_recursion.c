c518707eed8fe161a075ff3abf25e28da679ac15
MTHALO <mohammed.tegegne.35@gmail.com>
2022-07-21 09:05:52 -0700

    C - Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,6 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number.
- * power_operation - returns the natural square root of a number.
  * @n: input number.
  * @c: iterator.
  * Return: square root or -1.
